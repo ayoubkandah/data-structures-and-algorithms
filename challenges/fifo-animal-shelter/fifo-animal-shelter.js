26d12f69ed5fe5462806ed7eda74163d127bf647
ayoubkandah <a.kandah994@gmail.com>
2021-04-12 19:14:24 +0300

    code 12

--- challenges/fifo-animal-shelter/fifo-animal-shelter.js
+++ challenges/fifo-animal-shelter/fifo-animal-shelter.js
@@ -5,7 +5,7 @@ class Node{
     }
     
     }
-    class AnimalShelter{
+    class Queue{
     constructor(){
       this.front=null
       this.end=null
@@ -27,34 +27,95 @@ class Node{
     }
     
     }
-    deQueue(kind){
-      console.log(this.length)
-      let temp=this.front
-for(let x=0;x<this.length;x++){
-if(temp.value.name==kind){
+    deQueue(){
+      let result;
+      if(this.length>=1){
+result=this.front
+    this.front=this.front.next
+    this.length--
+    return result.value
+    }else if(this.length==1){
+    this.front=null
+    this.end=null
+    this.length--
+    result=null
+    return result
+    }else{
+      return "Exception"
+    }
+    
+    }
+    peek(){
+    if(this.length>0){
+    return this.front.value
+    
+    }else{
+      return "Exception"
+    }
+    
+    }
+    
+    isEmpty(){
+    if(this.length>0){
+      return false
+    }else{
+      return true
+    }
+    
+    }
+    
+    }
 
-return temp.value.constructor.name
+    class AnimalShelter{
+    constructor(){
+      this.dog=new Queue() ;
+      this.cat=new Queue();
+    }
+    
+    enQueue(val){
+      // console.log(val.constructor.name)
+      if(val.constructor.name=="dog"){
+      this.dog.enQueue(val)
+    }else if(val.constructor.name=="cat"){
+      this.cat.enQueue(val)
+    }else{console.log("ee")
+  throw new Error}
+    
+    }
+
+    
+    
+    deQueue(kind){
+      // console.log(this.length)
+      if(kind=="dog"){
+// console.log(this.dog.deQueue())
+return this.dog.deQueue()
+      }else if(kind=="cat"){
+
+        return this.cat.deQueue()
+      }else{
+          return null
+      }
+
+    
+
 }
 
-temp=temp.next
-}return null
-    }
     
     
     }
 
-    class Dog{
+    class dog{
         constructor(name)
         {
           this.name=name
         }
       }
-      class Cat{
+      class cat{
         constructor(name){
       this.name=name
       
         }
       }
 
-
-module.exports={AnimalShelter,Dog,Cat}
+      module.exports={dog,cat,AnimalShelter}
--- challenges/fifo-animal-shelter/fifo-animal-shelter.test.js
+++ challenges/fifo-animal-shelter/fifo-animal-shelter.test.js
@@ -1,45 +1,49 @@
-let {AnimalShelter}=require("./fifo-animal-shelter")
-let {Dog}=require("./fifo-animal-shelter")
-let {Cat}=require("./fifo-animal-shelter")
+let {AnimalShelter, cat,dog}=require("./fifo-animal-shelter")
+
 
 describe("testing",()=>{
     // console.log(LL.head,"0------------------------")
     test("enqueue animal dog and cat object",()=>{
         let Animal=new AnimalShelter()
-        let d=new Dog()
-        let d1=new Dog()
-        let cat=new Cat()
-        let cat1=new Cat()
-        cat.name="bella"
-        cat1.name="lucy"
+        let d=new dog()
+        // console.log(d,"eeeeeeeeeeeeee")
+        let d1=new dog()
+        let cat1=new cat()
+        let cat2=new cat()
+        cat1.name="bella"
+        cat2.name="lucy"
         d.name="rot"
         d1.name="husky"
-        Animal.enQueue(cat)
         Animal.enQueue(cat1)
+        Animal.enQueue(cat2)
         Animal.enQueue(d)
         Animal.enQueue(d1)
           
-        expect(Animal.length).toEqual(4)
-        expect(Animal.end.value.name).toEqual(d1.name)
-expect(Animal.front.value.name).toEqual(cat.name)
+        expect(Animal.cat.length).toEqual(2)
+        expect(Animal.dog.length).toEqual(2)
+// console.log(d1.name,"eeeeeeeeeeeeeeeeeee")
+        expect(Animal.dog.end.value.name).toEqual(d1.name)
+expect(Animal.cat.end.value.name).toEqual(cat2.name)
 
 })
-test("enqueue animal dog and cat object",()=>{
+test("dequeue animal dog and cat object",()=>{
     let Animal=new AnimalShelter()
-    let d=new Dog()
-    let d1=new Dog()
-    let cat=new Cat()
-    let cat1=new Cat()
-    cat.name="bella"
-    cat1.name="lucy"
+    let d=new dog()
+    let d1=new dog()
+    let cat1=new cat()
+    let cat2=new cat()
+    cat1.name="bella"
+    cat2.name="lucy"
     d.name="rot"
     d1.name="husky"
-    Animal.enQueue(cat)
     Animal.enQueue(cat1)
+    Animal.enQueue(cat2)
     Animal.enQueue(d)
     Animal.enQueue(d1)
-expect(Animal.deQueue("bella")).toEqual("Cat")
-expect(Animal.deQueue("xyz")).toEqual(null)
-expect(Animal.deQueue("husky")).toEqual("Dog")
+expect(Animal.deQueue("cat")).toEqual(cat1)
+expect(Animal.deQueue("dog")).toEqual(d)
+expect(Animal.deQueue("cat")).toEqual(cat2)
+expect(Animal.deQueue("dog")).toEqual(d1)
+expect(Animal.deQueue("E")).toEqual(null)
 })
 })
